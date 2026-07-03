5c62f855893ca12e4f7994068f9b8f5d17cd5f09
user-vio <uservio@proton.me>
2026-07-03 20:21:43 +0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "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";
-const MODEL = "deepseek-v4-flash-free";
-const ENDPOINT = "https://openrouter.ai/api/v1/chat/completions"
